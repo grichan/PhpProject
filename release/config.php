c9ce91842923c396d5541c5e8903c013563e7215
grichan <vladgrichan@gmail.com>
2018-01-26 17:18:32 +0200

    Overall Changes v3

--- release/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 1/10/2018
- * Time: 5:46 PM
- */
--- release/testRedirectLogin.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-if ( $_SESSION["logged_in"] == true){
-    echo (  $_SESSION["naam"]);
-} else {
-    echo ("NO SESSION ");
-}
